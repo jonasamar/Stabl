31d3da8c7579eb99e8d23fcd2ecb0575c08ea082
jonasamar <91546156+jonasamar@users.noreply.github.com>
2023-04-20 00:13:42 -0700

    Creation script test pour sherlock

---
+++ Test_Sherlock.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+import csv
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+df = pd.DataFrame(data = [['bonjour', 'Jonas', 'Amar'],[1.,2., 3.],['Test', 'is', 'OK']])
+
+df.to_csv(index=True)
+
+plt.plot(np.linspace(1, 2, 10), np.linspace(1, 2, 10))
+plt.title('Plot Test')
+plt.show()
+
